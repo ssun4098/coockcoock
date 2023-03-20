f903545594ac9a45238b7e7a7b16271f9fd0d3f0
bebo0 <bebo007@naver.com>
2023-03-20 20:03:22 +0900

    start

---
+++ src/main/java/com/coockcoock/shop/CoockcoockApplication.java
@@ -0,0 +1,13 @@
+package com.coockcoock.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoockcoockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoockcoockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coockcoock/shop/CoockcoockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coockcoock.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoockcoockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
