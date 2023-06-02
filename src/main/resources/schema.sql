45985dae97fa6d17f147ddcec2029f418c26de69
ssun4098 <bebo007@naver.com>
2023-06-03 00:34:35 +0900

    Feat Convert SQL
    회원과 게시물 다대다 레시피 추가 및 SQL 리팩토링

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,200 +1,105 @@
 drop table if exists `recipy_category`;
 drop table if exists `categories`;
-drop table if exists `great`;
+drop table if exists `write`;
 drop table if exists `comments`;
 drop table if exists `images`;
 drop table if exists `recipe_ingredient`;
 drop table if exists `ingredients`;
-drop table if exists `recipes`;
 drop table if exists `members`;
 drop table if exists `grades`;
-
-CREATE TABLE `members` (
-                           `id`	bigint	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                           `login_id`	varchar(32)	NOT NULL UNIQUE,
-                           `password`	varchar(256)	NOT NULL ,
-                           `sign_up_date`	date	NOT NULL ,
-                           `ban`	boolean	NOT NULL	DEFAULT false,
-                           `ban_reason`	varchar(256),
-                           `withdrawal`	boolean	NOT NULL	DEFAULT false,
-                           `grade_id`	bigint	NOT NULL
-);
-
-CREATE TABLE `recipes` (
-                           `id`	bigint	NOT NULL,
-                           `title`	varchar(256)	NOT NULL,
-                           `cookery`	varchar(1024)	NOT NULL,
-                           `write_date`	date	NULL,
-                           `score`	integer	NULL	DEFAULT 0,
-                           `create_datetime`	datetime	NULL,
-                           `update_datetime`	datetime	NOT NULL,
-                           `delete`	boolean	NULL,
-                           `writer`	bigint	NOT NULL
-);
+drop table if exists `recipes`;
 
 CREATE TABLE `grades` (
-                          `id`	bigint	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                          `name`	varchar(32)	NULL
+    `id`	bigint	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name`	varchar(32)	NULL
 );
 
 CREATE TABLE `ingredients` (
-                               `id`	bigint	NOT NULL,
-                               `name`	VARCHAR(255)	NULL
+    `id`	bigint	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name`	VARCHAR(255)	NULL
+);
+
+CREATE TABLE `members` (
+    `id`	bigint	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `login_id`	varchar(32)	NOT NULL UNIQUE,
+    `password`	varchar(256)	NOT NULL ,
+    `email` varchar(32) NOT NULL UNIQUE,
+    `sign_up_date`	date	NOT NULL ,
+    `ban`	boolean	NOT NULL	DEFAULT false,
+    `ban_reason`	varchar(256),
+    `withdrawal`	boolean	NOT NULL	DEFAULT false,
+    `grade_id`	bigint	NOT NULL
+);
+
+CREATE TABLE `recipes` (
+    `id`	bigint	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `title`	varchar(256)	NOT NULL,
+    `cookery`	varchar(1024)	NOT NULL,
+    `write_date`	date	NULL,
+    `score`	integer	NULL	DEFAULT 0,
+    `create_datetime`	datetime	NULL,
+    `update_datetime`	datetime	NOT NULL,
+    `delete`	boolean	NULL
 );
 
 CREATE TABLE `recipe_ingredient` (
-                                     `ingredient_id`	bigint	NOT NULL,
-                                     `recipe_id`	bigint	NOT NULL,
-                                     `amount`	varchar(32)	NULL
+    `ingredient_id`	bigint	NOT NULL,
+    `recipe_id`	bigint	NOT NULL,
+    `amount`	varchar(32)	NULL,
+    CONSTRAINT `mm_recipe`
+    foreign key (recipe_id)
+    REFERENCES recipes(id),
+    CONSTRAINT `mm_ingredient`
+    foreign key (ingredient_id)
+    REFERENCES ingredients(id)
 );
 
 CREATE TABLE `images` (
-                          `id`	bigint	NOT NULL,
-                          `name`	VARCHAR(255)	NULL,
-                          `recipe_id`	bigint	NOT NULL
+    `id`	bigint	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name`	VARCHAR(255)	NULL,
+    `recipe_id`	bigint	NOT NULL,
+    CONSTRAINT `image_recipe`
+    foreign key (recipe_id)
+    REFERENCES recipes(id)
 );
 
 CREATE TABLE `comments` (
-                            `id`	VARCHAR(255)	NOT NULL,
-                            `content`	varchar(256)	NULL,
-                            `depth`	integer	NULL,
-                            `edit_date`	date	NULL,
-                            `recipe_id`	bigint	NOT NULL,
-                            `member_id`	bigint	NOT NULL,
-                            `comment_id`	VARCHAR(255)	NULL
+    `id`	bigint	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `content`	varchar(256)	NULL,
+    `depth`	integer	NULL,
+    `edit_date`	date	NULL,
+    `recipe_id`	bigint	NOT NULL,
+    `member_id`	bigint	NOT NULL,
+    `comment_id` bigint	NULL,
+    foreign key (recipe_id)
+    REFERENCES recipes(id),
+    foreign key (member_id)
+    REFERENCES members(id),
+    foreign key (comment_id)
+    REFERENCES comments(id)
 );
 
-CREATE TABLE `great` (
-                         `member_id`	bigint	NOT NULL,
-                         `recipe_id`	bigint	NOT NULL,
-                         `enroll_date`	date	NULL
+CREATE TABLE `write` (
+    `member_id`	bigint	NOT NULL,
+    `recipe_id`	bigint	NOT NULL,
+    CONSTRAINT `write_recipe`
+    foreign key (recipe_id)
+    REFERENCES recipes(id),
+    CONSTRAINT `write_member`
+    foreign key (member_id)
+    REFERENCES members(id)
 );
 
 CREATE TABLE `categories` (
-                              `id`	bigint	NOT NULL,
-                              `name`	VARCHAR(255)	NULL
+    `id`	bigint	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name`	VARCHAR(255)	NULL
 );
 
-CREATE TABLE `recipy_category` (
-                                   `recipy_id`	bigint	NOT NULL,
-                                   `category_id`	bigint	NOT NULL
+CREATE TABLE `recipe_category` (
+    `recipe_id`	bigint	NOT NULL,
+    `category_id`	bigint	NOT NULL,
+    foreign key (recipe_id)
+    REFERENCES recipes(id),
+    foreign key (category_id)
+    REFERENCES categories(id)
 );
-
-ALTER TABLE `recipes` ADD CONSTRAINT `PK_RECIPES` PRIMARY KEY (
-                                                               `id`
-    );
-
-ALTER TABLE `ingredients` ADD CONSTRAINT `PK_INGREDIENTS` PRIMARY KEY (
-                                                                       `id`
-    );
-
-ALTER TABLE `recipe_ingredient` ADD CONSTRAINT `PK_RECIPE_INGREDIENT` PRIMARY KEY (
-                                                                                   `ingredient_id`,
-                                                                                   `recipe_id`
-    );
-
-ALTER TABLE `images` ADD CONSTRAINT `PK_IMAGES` PRIMARY KEY (
-                                                             `id`
-    );
-
-ALTER TABLE `comments` ADD CONSTRAINT `PK_COMMENTS` PRIMARY KEY (
-                                                                 `id`
-    );
-
-ALTER TABLE `great` ADD CONSTRAINT `PK_GREAT` PRIMARY KEY (
-                                                           `member_id`,
-                                                           `recipe_id`
-    );
-
-ALTER TABLE `categories` ADD CONSTRAINT `PK_CATEGORIES` PRIMARY KEY (
-                                                                     `id`
-    );
-
-ALTER TABLE `recipy_category` ADD CONSTRAINT `PK_RECIPY_CATEGORY` PRIMARY KEY (
-                                                                               `recipy_id`,
-                                                                               `category_id`
-    );
-
-ALTER TABLE `members` ADD CONSTRAINT `FK_grades_TO_members_1` FOREIGN KEY (
-                                                                           `grade_id`
-    )
-    REFERENCES `grades` (
-                         `id`
-        );
-
-ALTER TABLE `recipes` ADD CONSTRAINT `FK_members_TO_recipes_1` FOREIGN KEY (
-                                                                            `writer`
-    )
-    REFERENCES `members` (
-                          `id`
-        );
-
-ALTER TABLE `recipe_ingredient` ADD CONSTRAINT `FK_ingredients_TO_recipe_ingredient_1` FOREIGN KEY (
-                                                                                                    `ingredient_id`
-    )
-    REFERENCES `ingredients` (
-                              `id`
-        );
-
-ALTER TABLE `recipe_ingredient` ADD CONSTRAINT `FK_recipes_TO_recipe_ingredient_1` FOREIGN KEY (
-                                                                                                `recipe_id`
-    )
-    REFERENCES `recipes` (
-                          `id`
-        );
-
-ALTER TABLE `images` ADD CONSTRAINT `FK_recipes_TO_images_1` FOREIGN KEY (
-                                                                          `recipe_id`
-    )
-    REFERENCES `recipes` (
-                          `id`
-        );
-
-ALTER TABLE `comments` ADD CONSTRAINT `FK_recipes_TO_comments_1` FOREIGN KEY (
-                                                                              `recipe_id`
-    )
-    REFERENCES `recipes` (
-                          `id`
-        );
-
-ALTER TABLE `comments` ADD CONSTRAINT `FK_members_TO_comments_1` FOREIGN KEY (
-                                                                              `member_id`
-    )
-    REFERENCES `members` (
-                          `id`
-        );
-
-ALTER TABLE `comments` ADD CONSTRAINT `FK_comments_TO_comments_1` FOREIGN KEY (
-                                                                               `comment_id`
-    )
-    REFERENCES `comments` (
-                           `id`
-        );
-
-ALTER TABLE `great` ADD CONSTRAINT `FK_members_TO_great_1` FOREIGN KEY (
-                                                                        `member_id`
-    )
-    REFERENCES `members` (
-                          `id`
-        );
-
-ALTER TABLE `great` ADD CONSTRAINT `FK_recipes_TO_great_1` FOREIGN KEY (
-                                                                        `recipe_id`
-    )
-    REFERENCES `recipes` (
-                          `id`
-        );
-
-ALTER TABLE `recipy_category` ADD CONSTRAINT `FK_recipes_TO_recipy_category_1` FOREIGN KEY (
-                                                                                            `recipy_id`
-    )
-    REFERENCES `recipes` (
-                          `id`
-        );
-
-ALTER TABLE `recipy_category` ADD CONSTRAINT `FK_categories_TO_recipy_category_1` FOREIGN KEY (
-                                                                                               `category_id`
-    )
-    REFERENCES `categories` (
-                             `id`
-        );
